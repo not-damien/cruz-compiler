46d9fff8015838fc29c9daeb191635318fb4db97
Damien <damiencruzofficial@gmail.com>
2023-09-02 01:36:36 -0700

    added shell commmands to rust to assemble and link assembly code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
 use std::{fs::{File, self}, io::{Read, Write}};
+use std::process::Command;
 fn main() {
+    //shell commands 
+    //nasm -felf64 test.asm && ld test.o && ./a.out
+
     let mut f = File::create("foo.txt").expect("foo this shiz");
-    f.write_all(b"i am writing this as a test \nthis should be on a new line").expect("writing failed");
+    f.write_all(b"i am writing this as a test \nthis should be on a new lineeferfefefrerferff").expect("writing failed");
     println!("Hello, world!");
     let data = fs::read_to_string("./test.crz").expect("unable to open le file");
+    
+    
+    
+    //command to assemble 
+    Command::new("nasm")
+    .arg("-felf64")
+    .arg("test.asm")
+    .output()
+    .expect("failed nasm");
+
+    //commmand to link to libs 
+    Command::new("ld")
+    .arg("test.o")
+    .output()
+    .expect("failed linker");
+
+    //run program
+   // Command::new("./a.out").output().expect("failed to execute");
+
+
+
     println!("{}",data);
 }
