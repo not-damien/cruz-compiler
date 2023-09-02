591b3cf8b15fa924097c3154eeee2c4f037f9aac
not-damien <93546644+not-damien@users.noreply.github.com>
2023-09-01 18:01:58 -0700

    files can be made now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::{fs::{File, self}, io::{Read, Write}};
 fn main() {
+    let mut f = File::create("foo.txt").expect("foo this shiz");
+    f.write_all(b"i am writing this as a test \nthis should be on a new line").expect("writing failed");
     println!("Hello, world!");
+    let data = fs::read_to_string("./test.crz").expect("unable to open le file");
+    println!("{}",data);
 }
