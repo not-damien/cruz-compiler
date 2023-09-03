890fe95a19f74e8a6066cc53c2f0adb36bb633ee
Damien <damiencruzofficial@gmail.com>
2023-09-02 18:24:17 -0700

    started work on tokenizer and wirting to output file

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
 use std::{fs::{File, self}, io::{Read, Write}};
 use std::process::Command;
-fn main() {
-    //shell commands 
-    //nasm -felf64 test.asm && ld test.o && ./a.out
 
-    let mut f = File::create("foo.txt").expect("foo this shiz");
-    f.write_all(b"i am writing this as a test \nthis should be on a new lineeferfefefrerferff").expect("writing failed");
-    println!("Hello, world!");
+#[derive(Debug)]
+enum Token{
+    Tlet,
+    Tadd,
+    Tint,
+    Tsemi
+}
+
+
+fn main() {
+
+    
+    //read from input file
+    
     let data = fs::read_to_string("./test.crz").expect("unable to open le file");
     
     
+
+
+
+    let mut f = File::create("bar.asm").expect("asm file creation failed");
+
+    let tokens = parse(&data);
+    println!("{:?}", tokens);
     
+    let mut output = String::new();
+    buildstr(&mut output);
+    f.write_all(output.as_bytes()).expect("writing failed");
+
+
+
     //command to assemble 
     Command::new("nasm")
     .arg("-felf64")
-    .arg("test.asm")
+    .arg("bar.asm")
     .output()
     .expect("failed nasm");
 
     //commmand to link to libs 
     Command::new("ld")
-    .arg("test.o")
+    .arg("bar.o")
     .output()
     .expect("failed linker");
 
@@ -30,4 +51,60 @@ fn main() {
 
 
     println!("{}",data);
+}
+
+
+
+fn buildstr (str: &mut String){
+    str.push_str("global _start\n\n");
+    str.push_str("section .text\n\n");
+    str.push_str("_start:\n");
+    str.push_str(" mov rax, 1\n");
+    str.push_str(" mov rdi, 1 \n");
+    str.push_str(" mov rsi, msg\n");
+    str.push_str(" mov rdx, msglen\n");
+    str.push_str(" syscall \n");
+    str.push_str(" mov rax, 60\n");
+    str.push_str(" mov rdi, 0\n");
+    str.push_str(" syscall \n");
+
+    str.push_str("section .rodata\n\
+    msg: db \"Hello, compiler!\", 10\n\
+    msglen: equ $ - msg");
+
+}
+
+
+fn parse (str: &String)-> Vec<Token>{
+    let mut res = Vec::new();
+
+
+    let mut buff = String::new();
+    let mut e = str.chars();
+    loop{
+        match e.next() {
+            Some(v) => {
+                if v != ' ' && v != '\n'{
+                    buff.push(v);
+                    
+                }else{
+                    println!("|{}|",buff);
+                    if buff == "let"{
+                        res.push(Token::Tlet)
+                    }else if buff == "+" {
+                        res.push(Token::Tadd);
+                    }else if buff == ";" {
+                        res.push(Token::Tsemi);
+                    }else if buff.parse::<u32>().is_ok() {
+                        res.push(Token::Tint);
+                    }
+                    buff.clear();
+                    continue
+                }
+                //println!("{}",v)
+            },
+            None => break
+        }
+    }
+    res
 }
